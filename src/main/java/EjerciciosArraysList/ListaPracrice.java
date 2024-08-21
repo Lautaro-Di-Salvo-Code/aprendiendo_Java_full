16f4bfebca115b8c2ca0581dc996e0120d2b26e6
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-08-21 20:33:23 -0300

    Nuevo cambio desde IntelliJ

--- src/main/java/EjerciciosArraysList/ListaPracrice.java
+++ src/main/java/EjerciciosArraysList/ListaPracrice.java
@@ -10,11 +10,13 @@ public class ListaPracrice {
     public static void main(String[] args) {
     List<ArrayClassPractice> arregloLista = new ArrayList<> ();
     List<ArrayClassPractice> arregloLista2 = new ArrayList<> ();
-    
-    
+    List<ArrayClassPractice> arregloLista3 = new ArrayList<> ();
+
+
         arregloLista.add( new ArrayClassPractice(" Roberto ", 23, 34.5));
         arregloLista2.add(new ArrayClassPractice(" Matias ", 45, 10.5));
-        
+        arregloLista3.add(new ArrayClassPractice(" Lautaro ", 26, 50));
+
         for (ArrayClassPractice arrayClassPractice : arregloLista) {
             System.out.println("El arreglo contiene: " 
                     + arrayClassPractice.getNombre()
@@ -22,7 +24,12 @@ public class ListaPracrice {
                     +arrayClassPractice.getPrecio());
         }
         for (ArrayClassPractice arrayClassPractice : arregloLista2) {
-            System.out.println("El arreglo contiene: " 
+            System.out.println("El arreglo2 contiene: "
+                    + arrayClassPractice.getNombre()
+                    + " cifra: "+ arrayClassPractice.getCifra()+ " Precio: "
+                    +arrayClassPractice.getPrecio());
+        } for (ArrayClassPractice arrayClassPractice : arregloLista3) {
+            System.out.println("El arreglo3 contiene: "
                     + arrayClassPractice.getNombre()
                     + " cifra: "+ arrayClassPractice.getCifra()+ " Precio: "
                     +arrayClassPractice.getPrecio());
