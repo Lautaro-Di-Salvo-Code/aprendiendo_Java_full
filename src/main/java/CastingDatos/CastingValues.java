2102b2c211d2867ad170a9249371e15bb27ae40b
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-08-26 08:28:03 -0300

    Practicando casteo de tipos

---
+++ src/main/java/CastingDatos/CastingValues.java
@@ -0,0 +1,15 @@
+package CastingDatos;
+
+public class CastingValues {
+    public static void main(String[] args) {
+    String cifra = "453543";
+
+    int saludoConvertido = Integer.parseInt(cifra);
+
+    double saludoConvertido2 = Double.parseDouble(cifra);
+
+
+        System.out.println(saludoConvertido);
+        System.out.println(saludoConvertido2);
+    }
+}
