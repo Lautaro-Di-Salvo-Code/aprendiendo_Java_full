2b0873ea9455ea412df3d41bcc71227e4d6d50f4
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-08-26 08:48:21 -0300

    Practicando casteo de tipos, mas

--- src/main/java/CastingDatos/CastingValues.java
+++ src/main/java/CastingDatos/CastingValues.java
@@ -3,13 +3,16 @@ package CastingDatos;
 public class CastingValues {
     public static void main(String[] args) {
     String cifra = "453543";
+    int algo = 6546;
 
     int saludoConvertido = Integer.parseInt(cifra);
-
     double saludoConvertido2 = Double.parseDouble(cifra);
+    String palabra = String.valueOf(algo);
 
 
-        System.out.println(saludoConvertido);
-        System.out.println(saludoConvertido2);
+
+        System.out.println("Entero "+ saludoConvertido);
+        System.out.println("Decimal "+ saludoConvertido2);
+        System.out.println("Texto "+ palabra);
     }
 }
